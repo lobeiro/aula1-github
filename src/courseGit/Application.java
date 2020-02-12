abddd663e09e1c091fadb471710cc8490b5deccc
lobeiro <marcelo.lobeiro@gmail.com>
2020-02-12 14:45:50 -0300

    Projeto criado

---
+++ src/courseGit/Application.java
@@ -0,0 +1,10 @@
+package courseGit;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module courseGit {
+}
